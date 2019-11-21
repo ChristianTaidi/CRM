31b5a3d9ddcfcd36a39e54cb0367ba099b95a694
Christian Karem Taidi Santana <c.taidi.2016@alumnos.urjc.es>
2019-11-21 18:25:48 +0100

    added html, js and css

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const path = require('path')
 const app = express();
 app.listen(443,()=>{
     console.log('Server started on port 443')
 })
 
+app.get('/', (req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./index.html'));
+})
+
+app.get('/assets/js/CRM.js',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./assets/js/CRM.js'))
+})
+
+
+app.get('/')
 
