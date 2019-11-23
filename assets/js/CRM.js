dce9cc46e3ed8077957c4753aa69b508ae7060c1
Ruben98Z <43741046+Ruben98Z@users.noreply.github.com>
2019-11-23 12:22:53 +0100

    added charts

--- assets/js/CRM.js
+++ assets/js/CRM.js
@@ -239,4 +239,29 @@ $(document).ready(function () {
         }
     }
 
+
+    
+
+    google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Año', 'Ganancias', 'Gastos'],
+          ['2013',  1000,      400],
+          ['2014',  1170,      460],
+          ['2015',  660,       1120],
+          ['2016',  1030,      540]
+        ]);
+
+        var options = {
+          title: 'Rendimiento de la empresa',
+          hAxis: {title: 'Año',  titleTextStyle: {color: '#333'}},
+          vAxis: {minValue: 0}
+        };
+
+        var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }      
+
 })
